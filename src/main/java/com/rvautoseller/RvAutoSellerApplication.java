45aa32a20b3797c01479972dc8c3090c30a310ac
Pal Roland <palroland2004@gmail.com>
2025-10-07 11:46:49 +0300

    First commit

---
+++ src/main/java/com/rvautoseller/RvAutoSellerApplication.java
@@ -0,0 +1,13 @@
+package com.rvautoseller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RvAutoSellerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RvAutoSellerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rvautoseller/RvAutoSellerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rvautoseller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RvAutoSellerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

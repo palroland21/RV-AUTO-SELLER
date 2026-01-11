79e07750cf12a76b8b949ff1e85fc192a804ad3e
Vasile Breban <vbreban09@gmail.com>
2026-01-11 19:08:27 +0200

    backend - update listing response to have username & telephone

--- RV-AUTO-SELLER/backend/src/main/java/com/rv_auto_seller/dto/response/ListingResponse.java
+++ RV-AUTO-SELLER/backend/src/main/java/com/rv_auto_seller/dto/response/ListingResponse.java
@@ -28,6 +28,8 @@ public class ListingResponse {
     private TransmissionType transmissionType;
     private String description;
     private Long userId;
+    private String username;
+    private String telephone;
 
     public ListingResponse(Listing listing) {
         this.id = listing.getId();
@@ -44,6 +46,15 @@ public class ListingResponse {
         this.VIN = listing.getVIN();
         this.description = listing.getDescription();
         this.userId = listing.getUser().getId();
+
+        if(listing.getUser() != null) {
+            this.username = listing.getUser().getUsername();
+            this.telephone = listing.getUser().getTelephone();
+        }else{
+            this.username = "Unknown Seller";
+            this.telephone = null;
+        }
+
     }
 
     // Getteri
@@ -105,5 +116,81 @@ public class ListingResponse {
 
     public TransmissionType getTransmissionType() {
         return transmissionType;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setImages(List<Image> images) {
+        this.images = images;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public void setBrand(CarBrand brand) {
+        this.brand = brand;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public void setType(CarType type) {
+        this.type = type;
+    }
+
+    public void setYearOfManufacture(int yearOfManufacture) {
+        this.yearOfManufacture = yearOfManufacture;
+    }
+
+    public void setFuelType(FuelType fuelType) {
+        this.fuelType = fuelType;
+    }
+
+    public void setHorsePower(int horsePower) {
+        this.horsePower = horsePower;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public void setVIN(String VIN) {
+        this.VIN = VIN;
+    }
+
+    public void setTransmissionType(TransmissionType transmissionType) {
+        this.transmissionType = transmissionType;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
     }
 }
